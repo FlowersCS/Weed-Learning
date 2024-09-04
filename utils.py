6f5474b2d00b7588b60511d2995e0cca389f6b85
FlowersCS <carlos.flores.p@utec.edu.pe>
2024-09-04 10:30:18 -0500

    update utils

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from tqdm.notebook import tqdm
 import torch
+import matplotlib.pyplot as plt
 
 def calculate_accuracy(y_pred, y):
     top_pred = y_pred.argmax(1, keepdim = True)
@@ -54,3 +55,110 @@ def epoch_time(start_time, end_time):
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
+
+#### LEARNING RATE CODE
+class ExponentialLR(_LRScheduler):
+    def __init__(self, optimizer, end_lr, num_iter, last_epoch=-1):
+        self.end_lr = end_lr
+        self.num_iter = num_iter
+        super(ExponentialLR, self).__init__(optimizer, last_epoch)
+    
+    def get_lr(self):
+        curr_iter = self.last_epoch
+        r = curr_iter / self.num_iter
+        return [base_lr * (self.end_lr / base_lr) ** r
+                for base_lr in self.base_lrs]
+
+class IteratorWrapper:
+    def __init___(self, iterator):
+        self.iterator = iterator
+        self._iterator = iter(iterator)
+        
+    def __next__(self):
+        try:
+            inputs, labels = next(self._iterator)
+        except StopIteration:
+            self._iterator = iter(self.iterator)
+            inputs, labels, *_ = next(self._iterator)
+        return inputs, labels
+
+    def get_batch(self):
+        return next(self)
+
+class LRFinder:
+    def __init__(self, model, optimizer, criterion, device):
+        self.model = model
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.device = device
+        
+        torch.save(model.state_dict(), 'init_params.pt')
+    
+    def range_test(self, iterator, end_lr=10, num_iter=100,
+                   smooth_f=0.05, diverge_th=5):
+        lrs = []
+        losses = []
+        best_loss = float('inf')
+        
+        lr_scheduler = ExponentialLR(self.optimizer, end_lr, num_iter)
+        iterator = IteratorWrapper(iterator)
+        
+        for iteration in range(num_iter):
+            loss = self._train_batch(iterator)
+            lrs.append(lr_scheduler.get_last_lr()[0])
+            
+            #update lr
+            lr_scheduler.step()
+            if iteration > 0:
+                loss = smooth_f * loss + (1 - smooth_f) * losses[-1]
+            
+            if loss < best_loss:
+                best_loss = loss
+            
+            losses.append(loss)
+            
+            if loss > diverge_th * best_loss:
+                print("Stopping early, the loss has diverged")
+                break
+        
+        self.model.load_state_dict(torch.load('init_params.pt'))
+        
+        return lrs, losses
+            
+    def _train_batch(self, iterator):
+        self.model.train()
+        self.optimizer.zero_grad()
+        
+        x, y = iterator.get_batch()
+        
+        x = x.to(self.device)
+        y = y.to(self.device)
+        
+        y_pred = self.model(x) #,_
+        
+        loss = self.criterion(y_pred, y)
+        
+        loss.backward()
+        
+        self.optimizer.step()
+        
+        return loss.item()
+    
+
+def plot_lr_finder(lrs, losses, skip_start=5, skip_end=5):
+
+    if skip_end == 0:
+        lrs = lrs[skip_start:]
+        losses = losses[skip_start:]
+    else:
+        lrs = lrs[skip_start:-skip_end]
+        losses = losses[skip_start:-skip_end]
+
+    fig = plt.figure(figsize=(16, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(lrs, losses)
+    ax.set_xscale('log')
+    ax.set_xlabel('Learning rate')
+    ax.set_ylabel('Loss')
+    ax.grid(True, 'both', 'x')
+    plt.show()
